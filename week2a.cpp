0a6d2eeea2410cb030db06f08d605475fb70f9b5
akhil3690 <54197708+akhil3690@users.noreply.github.com>
2019-08-24 00:14:07 -0500

    Add files via upload

---
+++ week2a.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the size of an array :\n";
+	cin>>n;
+	
+	int arr[20];
+	cout<<"Enter elements of an array:\n";
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	
+	cout<<"Enter k:";
+	int k;
+	cin>>k;
+	
+	if(k<=0)
+	{
+		cout<<"k cannot be less than or equal to zero";
+	}
+	
+	int min;
+	for(int i=0;i<k;i++){
+		min = i;
+		for(int j=i+1;j<n;j++){
+			if(arr[j]<arr[min]){
+				swap(arr[j],arr[min]);
+			}
+		}
+	}
+	cout<<"Kth Element is: "<<arr[k-1]<<endl;
+}	
